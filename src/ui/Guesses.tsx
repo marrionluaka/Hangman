f1cdfe60315b4a180bf52812303d22844eb6a569
Marrion Lulu <marrion91300@gmail.com>
2022-06-04 15:22:38 -0700

    Updating Guesses list styles

--- src/ui/Guesses.tsx
+++ src/ui/Guesses.tsx
@@ -1,13 +1,22 @@
 import React from 'react'
+import styled from 'styled-components'
+
+const ListContainer = styled.ul`
+  list-style: none;
+  display: flex;
+  justify-content: space-evenly;
+  padding: 1rem 0;
+  font-size: 1.25rem;
+`
 
 const Guesses = ({ guesses }: { guesses: string[] }): JSX.Element => (
-  <ul>
+  <ListContainer>
     {
       guesses.map((guess: string, i: number) => (
         <li key={`${guess}-${i}`} data-testid={`guess-${i}`}>{guess}</li>
       ))
     }
-  </ul>
+  </ListContainer>
 )
 
 export default Guesses
